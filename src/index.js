4add6ee1a02ada9d824577b6b10e7c100acb1c49
alienz <limingxia07@gmail.com>
2021-04-21 20:01:19 +1000

    react gallery without data update

--- src/index.js
+++ src/index.js
@@ -3,24 +3,21 @@ import ReactDOM from "react-dom";
 import axios from "axios";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
+const { REACT_APP_API_URI } = process.env;
+
 
 const ImagesGallery = () => {
     const [images, setImages] = React.useState(null);
 
     React.useEffect(() => {
         let shouldCancel = false;
-
         const call = async () => {
-            const response = await axios.get(
-                "https://google-photos-album-demo2.glitch.me/4eXXxxG3rYwQVf948"
-            );
-            if (!shouldCancel && response.data && response.data.length > 0) {
-                setImages(
-                    response.data.map(url => ({
-                        original: `${url}=w1024`,
-                        // thumbnail: `${url}=w100`
-                    }))
-                );
+            const response = await axios.get(REACT_APP_API_URI);
+            if (!shouldCancel && response.data.posts && response.data.posts.length > 0) {
+                const imageList = response.data.posts.map(post => ({
+                    original: post.url
+                }))
+                setImages(imageList)
             }
         };
         call();
