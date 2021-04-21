fcaab4a026453cc102701548e737fa01e0a3d597
alienz <limingxia07@gmail.com>
2021-04-21 20:52:39 +1000

    retrieve page onSlide

--- src/index.js
+++ src/index.js
@@ -5,9 +5,25 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
 const { REACT_APP_API_URI } = process.env;
 
+// pagination
+const PAGE_LIMIT = 10;
 
 const ImagesGallery = () => {
     const [images, setImages] = React.useState(null);
+    const [fetchedMaxPage, setFetchedMaxPage] = React.useState(1);
+    const  onSlide = async (currentIndex) => {
+        // last three item fetch new
+        let currentPage = Math.floor(currentIndex/PAGE_LIMIT) + 1
+        if (((currentIndex + 3) % PAGE_LIMIT === 0) && (fetchedMaxPage>=currentPage)) {
+            // fetch new page and append to the list
+            console.log('Fetching data for next page')
+            // if successful update max page
+            const response = await axios.get(REACT_APP_API_URI);
+            setFetchedMaxPage(fetchedMaxPage+1)
+            console.log(`${fetchedMaxPage} pages retrieved`)
+        }
+        console.log(currentIndex)
+    }
 
     React.useEffect(() => {
         let shouldCancel = false;
@@ -26,6 +42,8 @@ const ImagesGallery = () => {
 
     return images ? <ImageGallery items={images}
                                   showThumbnails={false}
+                                  lazyLoad
+                                  onSlide={onSlide}
     /> : null;
 };
 
