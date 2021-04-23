1cbb95ecd2f73c881524d92c8a7bfa9ec58dfc4f
alienz <limingxia07@gmail.com>
2021-04-24 09:53:35 +1000

    re-organize actions

---
+++ src/actions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+const { REACT_APP_API_URI, REACT_APP_PAGE_LIMIT } = process.env;
+
+export const getData = (page) => {
+    console.log(`Fetching data for page ${page}`)
+    return dispatch => {
+        axios.get(`${REACT_APP_API_URI}?page=${page}&limit=${REACT_APP_PAGE_LIMIT}`).then(res =>
+            dispatch({
+                type: "FETCH_DATA",
+                data: {...res.data,
+                    fetchedMaxPage: page
+                }
+            })
+        );
+    };
+}
+
+export const updateIndex = (currentIndex) => {
+    console.log(`Updating index as ${currentIndex}`)
+    return dispatch => {
+        dispatch({
+            type: "UPDATE_INDEX",
+            data: {
+                currentIndex
+            }
+        })
+    };
+}
--- src/app.js
+++ src/app.js
@@ -1,37 +1,11 @@
 import React, {useEffect, useRef} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
-const { REACT_APP_API_URI } = process.env;
-const PAGE_LIMIT = 50;
+import { getData, updateIndex } from "./actions";
 
+const { REACT_APP_PAGE_LIMIT } = process.env;
 
-const getData = (page) => {
-    console.log(`Fetching data for page ${page}`)
-    return dispatch => {
-        axios.get(`${REACT_APP_API_URI}?page=${page}&limit=${PAGE_LIMIT}`).then(res =>
-            dispatch({
-                type: "FETCH_DATA",
-                data: {...res.data,
-                        fetchedMaxPage: page
-                }
-            })
-        );
-    };
-}
-
-const updateIndex = (currentIndex) => {
-    console.log(`Updating index as ${currentIndex}`)
-    return dispatch => {
-            dispatch({
-                type: "UPDATE_INDEX",
-                data: {
-                    currentIndex
-                }
-            })
-    };
-}
 
 const onImageLoad = (event) => {
     const {target: { clientWidth, clientHeight }} = event
@@ -58,8 +32,8 @@ const App = () => {
         let fetchedCurrentIndex = controllerRef.current.getCurrentIndex()
         // console.log(`Current Index as ${currentIndex}`)
         const swipeRight = nextIndex === 0 || nextIndex > fetchedCurrentIndex
-        let currentPage = Math.floor(nextIndex/PAGE_LIMIT) + 1
-        if (((currentIndex + 3) % PAGE_LIMIT === 0) && (fetchedMaxPage === currentPage)) {
+        let currentPage = Math.floor(nextIndex/REACT_APP_PAGE_LIMIT) + 1
+        if (((currentIndex + 3) % REACT_APP_PAGE_LIMIT === 0) && (fetchedMaxPage === currentPage)) {
             // fetch new page and append to the list
             // if successful update max page
             if (swipeRight) {
