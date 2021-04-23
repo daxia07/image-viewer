a70a871c1b29c82418c1a297e079a76d2df59426
alienz <limingxia07@gmail.com>
2021-04-24 09:39:44 +1000

    aligin center

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import thunk from "redux-thunk";
 import reducer from "./store";
 import { Provider } from "react-redux";
 import App from "./app";
+import './index.css'
 
 const store = createStore(reducer, applyMiddleware(thunk))
 
